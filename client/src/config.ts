b59b60e1b0e56844592ff853b400709ab550f704
Freeman Jiang <freeman.jiang.ca@gmail.com>
2024-02-02 02:47:11 -0500

    adjust base url on env

--- client/src/config.ts
+++ client/src/config.ts
@@ -1,6 +1,6 @@
 import baseAxios from "axios";
 
-export const BASE_URL = "localhost:8000";
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "localhost:8000";
 
 export const customAxios = baseAxios.create({
   baseURL: `http://${BASE_URL}`,
